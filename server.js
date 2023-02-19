84cf9f7a0d9617c3ed8c50d49059ca9a4246fffa
Elijah Darkeh <elijahagbedam18@gmail.com>
2023-02-19 16:17:55 +0000

    improvements

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const DB_URL = "mongodb+srv://darkeh1807:alisajunior@cluster0.gyzovmf.mongodb.net/?retryWrites=true&w=majority";
+
 const server = express()
 const bodyParser = require("body-parser")
 const Student = require("./models/student")
-const functions = require("firebase-functions");
 server.use(bodyParser.json());
 
 
-mongoose.set('strictQuery', false);
-    mongoose.connect(DB_URL, { useNewUrlParser: true }).then(
-        function () {
-            console.log("Database is connected successfully");
-        }
-    ).
-        catch(
-            function (e) {
-                console, log(e)
-            }
-        )
+
 //POST STUDENT
 server.post("/students", async (req, res) => {
     const student = new Student(req.body);
@@ -47,7 +37,7 @@ server.get("/students/:id", async (req, res) => {
     try {
         const student = await Student.findOne({ id: req.params.id });
         if (!student) {
-            return res.send(student)
+            return res.send("Student does not exist")
         }
         res.send(student)
     } catch (error) {
@@ -55,9 +45,23 @@ server.get("/students/:id", async (req, res) => {
     }
 
 })
+//CONNECT TO DATABASE
+mongoose.set('strictQuery', false);
+mongoose.connect(DB_URL, { useNewUrlParser: true }).then(
+    function () {
+        console.log("Database is connected successfully");
+    }
+).
+    catch(
+        function (e) {
+            console, log(e)
+        }
+    )
 
-server.listen( 2000, function () {
-    console.log("Server is running at port 2000");
-    
+//START SERVER
+const PORT = process.env.PORT || 2000;
+server.listen(PORT, function () {
+    console.log(`Server is running at port ${PORT}`);
+
 })
 
